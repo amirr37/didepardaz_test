f366686e2907bc51f614ab6dc36d635aa741ac80
amir37 <amirrj037@gmail.com>
2024-09-29 17:51:02 +0330

    create base of admin classes of Phone-Country-Brand

--- phone/admin.py
+++ phone/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from phone.models import Country, Phone, Brand
+
 
 # Register your models here.
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    pass
